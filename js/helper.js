a99cc880811f71c95e349b984b6199fb005e0248
Les Ansley <les@ansleyfamily.com>
2015-08-14 11:30:36 +0100

    add initial files

---
+++ js/helper.js
@@ -0,0 +1,257 @@
+/*
+This file contains all of the code running in the background that makes resumeBuilder.js possible. We call these helper functions because they support your code in this course.
+Don't worry, you'll learn what's going on in this file throughout the course. You won't need to make any changes to it until you start experimenting with inserting a Google Map in Problem Set 3.
+Cameron Pittman
+*/
+
+
+/*
+These are HTML strings. As part of the course, you'll be using JavaScript functions
+replace the %data% placeholder text you see in them.
+*/
+var HTMLheaderName = '<header class="row"><div class="col-headerName"><h1><span id="firstname">%first%</span><span id="lastname">%last%</span></h1></div>';
+var HTMLheaderRole = '<div class="col-headerRole role-div"><span class="headerRole">%data%</span></div></header><hr class="header-divider"/>';
+
+var HTMLcontactGeneric = '<li class="flex-item contact-item"><span class="orange-text">%contact%</span><span class="contact-text"><a class="contact-link" target="_blank" href="%link%">%data%</a></span></li>';
+
+var HTMLbioPic = '<img src="%data%" class="biopic mobile-invisible">';
+var HTMLwelcomeMsg = '<span class="welcome-message">%data%</span>';
+
+var HTMLskillsStart = '<h3 id="skills-title" class="mobile-invisible">Skills at a Glance:</h3><ul id="skills-list" class="flex-box"></ul>';
+var HTMLskills = '<li class="flex-item skill-item"><span class="skills-text">%data%</span></li>';
+
+var HTMLskillsSectionContainer = '<section id="skills-section-container"></section>'
+var HTMLskillsSectionStart = '<ul id="skills-section-list" class="flex-box"></ul>'
+var HTMLskillsSectionContent = '<li class="flex-item skill-section-item"><span class="skills-section-text">%data%</span></li>'
+
+
+var HTMLworkContainer ='<section id="work-container"></section>'
+var HTMLworkStart = '<div class="work-entry row"></div>';
+var HTMLworkEmployer = '<div class="col-2 workPlace"><a class="workPlace"href="#">%data%</a></div>';
+var HTMLworkRole = '<div class="work-role col-10">%data%</div>';
+var HTMLworkDates = '<div class="date-text col-8">%data%</div>';
+var HTMLworkLocation = '<div class="location-text col-4">%data%</div>';
+var HTMLworkDescription = '<div class="work-description col-12">%data%</div>';
+
+var HTMLprojectContainer ='<section id="project-container"></section>'
+var HTMLprojectStart = '<div class="project-entry row"></div>';
+var HTMLprojectTitle = '<a class="projectTitle" href="#">%data%</a>';
+var HTMLprojectDates = '<div class="date-text">%data%</div>';
+var HTMLprojectDescription = '<div>%data%</div>';
+var HTMLprojectImage = '<figure><img class="imgResponsive" src="%data%"></figure>';
+
+var HTMLschoolContainer ='<section id="school-container"></section>'
+var HTMLschoolStart = '<div class="education-entry row"></div>';
+var HTMLschoolName = '<div class="col-3 schoolPlace"><a class="schoolPlace" href="#">%data%</a></div>';
+var HTMLschoolDegree = '<div class="col-10">%data%</div>';
+var HTMLschoolDates = '<div class="date-text col-8">%data%</div>';
+var HTMLschoolLocation = '<div class="location-text col-4">%data%</div>';
+var HTMLschoolMajor = '<div class="school-major col-12"><em>%data%</em></div>';
+
+var HTMLonlineClasses = '<h3 class="subSection">Online Classes</h3>';
+var HTMLonlineTitle = '<div class="col-12"><a class="onlineCourse" href="%url%" target="_blank">%data%</a></div>';
+var HTMLonlineSchool = '<div class="col-12 online-school">%data%</div>';
+var HTMLonlineDates = '<div class="col-12 date-text">%data%</div>';
+
+var HTMLpublicationContainer = '<section id="publication-container"></section>';
+var HTMLpublicationStart = '<div class="publication-entry row"></div>';
+var HTMLpublicationAuthor = '%data%, ';
+var HTMLpublicationLastAuthor = '%data%.';
+var HTMLpublicationTitle = ' \'%data%\' ';
+var HTMLpublicationJournal = '%data%, ';
+var HTMLpublicationVolume = '%data%';
+var HTMLpublicationIssue = '(%data%):';
+var HTMLpublicationPages = ' %data%, ';
+var HTMLpublicationDate = '%data%.';
+
+var HTMLpublication = '<a class="reference" href="%data%" target="_blank">%authors%. \'%title%\' %journal%,%volume%%issue%%pages%%date%</div>'
+var HTMLformattedPublication = '%authors%.&nbsp\'%title%\'&nbsp<i>%journal%</i>,&nbsp<b>%volume%</b>(%issue%):&nbsp%pages%,&nbsp%date%.'
+
+var HTMLcontactContainer = '<section id="contact-container"></section>'
+var HTMLcontactStart = '<div class="contact-entry row"></div>'
+var HTMLcontactChannel = '<div class="contact-channel col-2 orange-text">%data%:</div>'
+var HTMLcontactInfo = '<div class="contact-info col-9"><a class="contact-link" target="_blank" href="%link%">%data%</a></div>'
+
+var internationalizeButton = '<button>Internationalize</button>';
+
+var HTMLmapContainer ='<div id="map-container"></div>'
+var googleMap = '<section id="map"></section>';
+
+var HTMLframeworkDiv = '<div class="col-%num% framework-entry"></div>'
+
+/*
+The International Name challenge in Lesson 2 where you'll create a function that will need this helper code to run. Don't delete! It hooks up your code to the button you'll be appending.
+*/
+$(document).ready(function() {
+  $('button').click(function() {
+    var iName = inName() || function(){};
+    $('#name').html(iName);
+  });
+});
+
+/*
+The next few lines about clicks are for the Collecting Click Locations quiz in Lesson 2.
+*/
+clickLocations = [];
+
+function logClicks(x,y) {
+  clickLocations.push(
+    {
+      x: x,
+      y: y
+    }
+  );
+  console.log('x location: ' + x + '; y location: ' + y);
+}
+
+$(document).click(function(loc) {
+  // your code goes here!
+});
+
+
+//var publications = http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&retmode=json&retmax=1000&term=ansley+l[author]
+
+/*
+This is the fun part. Here's where we generate the custom Google Map for the website.
+See the documentation below for more details.
+https://developers.google.com/maps/documentation/javascript/reference
+*/
+var map;    // declares a global map variable
+
+
+/*
+Start here! initializeMap() is called when page is loaded.
+*/
+function initializeMap() {
+
+  var locations;
+
+  var mapOptions = {
+    disableDefaultUI: true
+  };
+
+  // This next line makes `map` a new Google Map JavaScript Object and attaches it to
+  // <div id="map">, which is appended as part of an exercise late in the course.
+  map = new google.maps.Map(document.querySelector('#map'), mapOptions);
+
+
+  /*
+  locationFinder() returns an array of every location string from the JSONs
+  written for bio, education, and work.
+  */
+  function locationFinder() {
+
+    // initializes an empty array
+    var locations = [];
+
+    // iterates through school locations and appends each location to
+    // the locations array
+    for (var school in education.schools) {
+      locations.push(education.schools[school].location);
+    }
+
+    // iterates through work locations and appends each location to
+    // the locations array
+    for (var job in work.jobs) {
+      locations.push(work.jobs[job].location);
+    }
+
+    return locations;
+  }
+
+  /*
+  createMapMarker(placeData) reads Google Places search results to create map pins.
+  placeData is the object returned from search results containing information
+  about a single location.
+  */
+  function createMapMarker(placeData) {
+
+    // The next lines save location data from the search result object to local variables
+    var lat = placeData.geometry.location.lat();  // latitude from the place service
+    var lon = placeData.geometry.location.lng();  // longitude from the place service
+    var name = placeData.formatted_address;   // name of the place from the place service
+    var bounds = window.mapBounds;            // current boundaries of the map window
+
+    // marker is an object with additional data about the pin for a single location
+    var marker = new google.maps.Marker({
+      map: map,
+      position: placeData.geometry.location,
+      title: name
+    });
+
+    // infoWindows are the little helper windows that open when you click
+    // or hover over a pin on a map. They usually contain more information
+    // about a location.
+    var infoWindow = new google.maps.InfoWindow({
+      content: name
+    });
+
+    // fires off an event when the user clicks on the map
+    google.maps.event.addListener(marker, 'click', function() {
+      infoWindow.open(map, marker);
+    });
+
+    // this is where the pin actually gets added to the map.
+    // bounds.extend() takes in a map location object
+    bounds.extend(new google.maps.LatLng(lat, lon));
+    // fit the map to the new marker
+    map.fitBounds(bounds);
+    // center the map
+    map.setCenter(bounds.getCenter());
+  }
+
+  /*
+  callback(results, status) makes sure the search returned results for a location.
+  If so, it creates a new map marker for that location.
+  */
+  function callback(results, status) {
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+      createMapMarker(results[0]);
+    }
+  }
+
+  /*
+  pinPoster(locations) takes in the array of locations created by locationFinder()
+  and fires off Google place searches for each location
+  */
+  function pinPoster(locations) {
+
+    // creates a Google place search service object. PlacesService does the work of
+    // actually searching for location data.
+    var service = new google.maps.places.PlacesService(map);
+
+    // Iterates through the array of locations, creates a search object for each location
+    for (var place in locations) {
+
+      // the search request object
+      var request = {
+        query: locations[place]
+      };
+
+      // Actually searches the Google Maps API for location data and runs the callback
+      // function with the search results after each search.
+      service.textSearch(request, callback);
+    }
+  }
+
+  // Sets the boundaries of the map based on pin locations
+  window.mapBounds = new google.maps.LatLngBounds();
+
+  // locations is an array of location strings returned from locationFinder()
+  locations = locationFinder();
+
+  // pinPoster(locations) creates pins on the map for each location in
+  // the locations array
+  pinPoster(locations);
+
+}
+
+// Calls the initializeMap() function when the page loads
+window.addEventListener('load', initializeMap);
+
+// Vanilla JS way to listen for resizing of the window
+// and adjust map bounds
+window.addEventListener('resize', function(e) {
+  // Make sure the map bounds get updated on page resize
+  map.fitBounds(mapBounds);
+});
---
+++ js/resumeBuilder.js
@@ -0,0 +1,505 @@
+var bio = {
+	firstname: 'Les',
+	lastname: 'Ansley',
+	role : 'Research Scientist',
+	contacts: {
+		email: ['les@ansleyfamily.com','mailto:les@ansleyfamily.com'],
+		mobile: ['+44 7999 418 119','tel:+447999418119'],
+		twitter: ['@dr_les', 'https://twitter.com/dr_les'],
+		linkedin: ['lesansley','https://uk.linkedin.com/in/lesansley'],
+		google: ['+lesansley', 'https://plus.google.com/+LesAnsley'],
+		github: ['lesansley','https://github.com/lesansley']
+	},
+	'bioPic': 'images/profile.jpg',
+	'welcomeMsg': 'After more than a decade building a successful academic career I am looking for new challenges that align with my enthusiam for technology and design.',
+	'skills': ['leadership','problem solving','analytical thinking','data analysis','public speaking','research','project management','technology','javascript','teaching','html','css']
+};
+
+var work = {
+	'jobs': [
+		{
+			employer: 'Northumbria University',
+			role: 'Associate Professor',
+			dates: 'Sep 2007 to Present',
+			location: 'Newcastle, UK',
+			description: 'blah-blah'
+		},
+		{
+			employer: 'Kingston University',
+			role: 'Senior Lecturer',
+			dates: 'Sep 2003 to Aug 2007',
+			location: 'London, UK',
+			description: 'blah-blah'
+		},
+		{
+			employer: 'Institut Montana',
+			role: 'Teacher',
+			dates: 'Sep 1998 to Aug 1999',
+			location: 'Zug, Switzerland',
+			description: 'blah-blah'
+		}
+	]
+};
+
+var project = {
+	'programs': [
+		{
+			title: 'Violet Project',
+			dates: 'Current',
+			description: 'blah',
+			image: 'images/holder.gif'
+		},
+		{
+			title: 'BaJIR',
+			dates: 'Current',
+			description: 'blah',
+			image: 'images/nhs.jpg'
+		},
+		{
+			title: 'Newcastle Falcons',
+			dates: 'November 2013',
+			description : 'blah',
+			image: 'images/falcons.png'
+		}
+	]
+};
+
+var education = {
+	'schools': [
+		{
+			name: 'Kingston University',
+			degree: 'Post-Graduate Teaching Certificate',
+			major: 'Higher Education',
+			location: 'London, UK',
+			dates: 'Sep 2004 to Jun 2005'
+		},
+		{
+			name: 'University of Cape Town',
+			degree: 'PhD',
+			major: 'Physiology',
+			location: 'Cape Town, South Africa',
+			dates: 'Jan 2000 to Mar 2003'
+		},
+		{
+			name: 'University of Birmingham',
+			degree: 'BSc (Hons)',
+			major: 'Sport & Exercise Science',
+			location: 'Birmingham, UK',
+			dates: 'Sep 1996 to Jun 1998'
+		}
+	],
+	'onlineCourses': [
+		{
+			title: 'Front-end Web Developer',
+			school: 'Udacity',
+			dates: 'Apr 2015 to Current',
+			url: 'https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001'
+		}
+	]
+};
+
+var publication = getPublications();
+
+//function to display bio
+bio.display = function() {
+
+	var formattedheaderRole = HTMLheaderRole.replace('%data%',bio.role);
+	var formattedheaderName = HTMLheaderName.replace('%first%',bio.firstname);
+	formattedheaderName = formattedheaderName.replace('%last%',bio.lastname);
+	var formattedNameRole = formattedheaderName + formattedheaderRole;
+	var formattedbioPic = HTMLbioPic.replace('%data%', bio.bioPic);
+	var formattedwelcomeMsg = HTMLwelcomeMsg.replace('%data%', bio.welcomeMsg);
+
+	$('#header').prepend(formattedNameRole);
+
+	//Add contact information
+
+	for(var contact in bio.contacts) {
+		var formattedContact = HTMLcontactGeneric.replace('%contact%', contact + ' ');
+		formattedContact = formattedContact.replace('%data%', bio.contacts[contact][0])
+		formattedContact = formattedContact.replace('%link%', bio.contacts[contact][1])
+
+		$('#topContacts').append(formattedContact);
+	}
+
+	//Add skills
+	$('#introduction').append(formattedbioPic);
+	$('#introduction').append(formattedwelcomeMsg);
+	if(bio.skills.length>0) {
+		$('#introduction').append(HTMLskillsStart);
+
+		for(var skill in bio.skills) {
+			var formattedSkill = HTMLskills.replace('%data%', bio.skills[skill]);
+			$('#skills-list').append(formattedSkill);
+		}
+	}
+};
+
+skills.display = function() {
+
+	$('#skills').append(HTMLskillsSectionContainer);
+
+	$('#skills-section-container').append(HTMLskillsSectionStart);
+	for(var skill in bio.skills) {
+			var formattedSectionSkill = HTMLskillsSectionContent.replace('%data%', bio.skills[skill]);
+			$('#skills-section-list').append(formattedSectionSkill);
+		}
+}
+
+contact.display = function() {
+	$('#contact').append(HTMLcontactContainer);
+
+	for(var contact in bio.contacts) {
+		$('#contact-container').append(HTMLcontactStart);
+		var formattedContactChannel = HTMLcontactChannel.replace('%data%', contact);
+		var formattedContactInfo = HTMLcontactInfo.replace('%data%',bio.contacts[contact][0]);
+		formattedContactInfo = formattedContactInfo.replace('%link%', bio.contacts[contact][1]);
+
+		$('.contact-entry:last').append(formattedContactChannel);
+		$('.contact-entry:last').append(formattedContactInfo);
+	}
+
+}
+
+//function to display work experience
+work.display = function() {
+
+	$('#workExperience').append(HTMLworkContainer);
+
+	for(var job in work.jobs) {
+
+		//Create div for work experience
+		$('#work-container').append(HTMLworkStart);
+
+		var formattedEmployer = HTMLworkEmployer.replace('%data%', work.jobs[job].employer);
+		var formattedRole = HTMLworkRole.replace('%data%', work.jobs[job].role);
+		var formattedDates = HTMLworkDates.replace('%data%',work.jobs[job].dates);
+		var formattedLocation = HTMLworkLocation.replace('%data%',work.jobs[job].location);
+		var formattedDescription = HTMLworkDescription.replace('%data%',work.jobs[job].description);
+
+		$('.work-entry:last').append(formattedEmployer);
+		$('.work-entry:last').append(formattedRole);
+		$('.work-entry:last').append(formattedDates);
+		$('.work-entry:last').append(formattedLocation);
+		$('.work-entry:last').append(formattedDescription);
+
+	}
+};
+
+//function to display  projects
+projects.display = function() {
+	//This variable sets the number of columns in the portfolio section
+	var columns = 3;
+	var index = 0;
+
+	$('#projects').append(HTMLprojectContainer);
+
+	while (index < project.programs.length) {
+
+		$('#project-container').append(HTMLprojectStart);
+
+		var iterations = frameWorkDivision(project.programs.length, index, columns);
+
+		for(var i=0; i<iterations; i++) {
+
+			var formattedframeworkDiv = HTMLframeworkDiv.replace('%num%', 12/columns);
+			$('.project-entry:last').append(formattedframeworkDiv);
+
+			var formattedTitle = HTMLprojectTitle.replace('%data%',project.programs[index].title);
+			var formattedDates = HTMLprojectDates.replace('%data%',project.programs[index].dates);
+			var formattedDescription = HTMLprojectDescription.replace('%data%',project.programs[index].description);
+			var formattedImage = HTMLprojectImage.replace('%data%',project.programs[index].image);
+
+			$('.framework-entry:last').append(formattedTitle);
+			$('.framework-entry:last').append(formattedDates);
+			$('.framework-entry:last').append(formattedDescription);
+			$('.framework-entry:last').append(formattedImage);
+
+			index++;
+		}
+	}
+};
+
+//function to display education
+education.display = function() {
+
+	$('#education').append(HTMLschoolContainer);
+
+	for(var school in education.schools) {
+
+		//Create div for work experience
+		$('#school-container').append(HTMLschoolStart);
+
+		var formattedName = HTMLschoolName.replace('%data%',education.schools[school].name);
+		var formattedDegree = HTMLschoolDegree.replace('%data%',education.schools[school].degree);
+		var formattedDates = HTMLschoolDates.replace('%data%',education.schools[school].dates);
+		var formattedLocation = HTMLschoolLocation.replace('%data%',education.schools[school].location);
+		var formattedMajor = HTMLschoolMajor.replace('%data%',education.schools[school].major);
+
+		var formattedSchoolDegree = formattedName + formattedDegree;
+
+		$('.education-entry:last').append(formattedName);
+		$('.education-entry:last').append(formattedDegree);
+		$('.education-entry:last').append(formattedDates);
+		$('.education-entry:last').append(formattedLocation);
+		$('.education-entry:last').append(formattedMajor);
+	}
+	$('#school-container').append(HTMLschoolStart);
+	$('.education-entry:last').append(HTMLonlineClasses);
+
+	for(var course in education.onlineCourses) {
+
+		$('#school-container').append(HTMLschoolStart);
+		var formattedonlineTitle = HTMLonlineTitle.replace('%data%',education.onlineCourses[course].title);
+		formattedonlineTitle = formattedonlineTitle.replace('%url%',education.onlineCourses[course].url);
+		var formattedonlineSchool = HTMLonlineSchool.replace('%data%',education.onlineCourses[course].school);
+		var formattedonlineDates = HTMLonlineDates.replace('%data%',education.onlineCourses[course].dates);
+
+		$('.education-entry:last').append(formattedonlineTitle);
+		$('.education-entry:last').append(formattedonlineDates);
+		$('.education-entry:last').append(formattedonlineSchool);
+	}
+};
+
+
+$('#mapDiv').append(googleMap);
+
+//Function calls
+bio.display();
+work.display();
+projects.display();
+education.display();
+skills.display();
+contact.display();
+
+//set the display of the sections to none
+document.getElementById('skills-section-container').style.display = 'none';
+document.getElementById('work-container').style.display = 'none';
+document.getElementById('project-container').style.display = 'none';
+document.getElementById('school-container').style.display = 'none';
+document.getElementById('contact-container').style.display = 'none';
+document.getElementById('map').style.display = 'none';
+
+//This function calculates how many portfolio images need to go on each line
+function frameWorkDivision(arrayLength, indexReached, columnsPerRow) {
+
+	var iterations = Math.floor((arrayLength - indexReached) / columnsPerRow);
+
+	if(iterations === 0) {
+		iterations = (arrayLength - indexReached) % columnsPerRow;
+	}
+	else {
+		iterations = columnsPerRow;
+	}
+	return iterations;
+}
+
+//preload images of the arrow icon for faster transition
+var openArrow = new Image();
+var closeArrow = new Image();
+var openArrowHover = new Image();
+var closeArrowHover = new Image();
+
+openArrow.src = 'images/arrow-down.png';
+closeArrow.src = 'images/arrow-right.png';
+openArrowHover.src = 'images/arrow-down-hover.png';
+closeArrowHover.src = 'images/arrow-right-hover.png';
+
+//set initial status for section dropdown: Closed = FALSE
+var workStatus, educationStatus, projectStatus, mapStatus, publicationStatus, skillsStatus, contactStatus= false;
+
+//change the arrow icon when hover starts
+function changeImage(buttonName, booleanName) {
+	if(booleanName) {
+		document.getElementById(buttonName).src = closeArrow.src;
+	}
+	else{
+		document.getElementById(buttonName).src = openArrow.src;
+	}
+	return true;
+}
+
+//return arrow icon to reflect current display status of the section
+function changeImageBack(buttonName, booleanName) {
+
+	if(booleanName) {
+		document.getElementById(buttonName).src = openArrow.src;
+	}
+	else{
+		document.getElementById(buttonName).src = closeArrow.src;
+	}
+	return true;
+}
+
+//toggle the display status of the section and arrow icon
+function handleMDown(buttonName, booleanName, elementName) {
+
+	var status = passFromStatus(booleanName);
+
+	if(status) {
+		document.getElementById(buttonName).src = closeArrow.src;
+		document.getElementById(elementName).style.display = 'none';
+	}
+	else {
+		document.getElementById(buttonName).src = openArrow.src;
+		document.getElementById(elementName).style.display = 'block';
+
+		if(elementName==='map') {
+			triggerResize();
+		}
+	}
+
+ 	status = !status;
+
+ 	passToStatus(booleanName, status);
+
+ 	return true;
+}
+
+function triggerResize() {
+	initializeMap();
+	google.maps.event.trigger(map, 'resize')
+}
+
+
+//passes the current display status of the section (i.e. none=false; block=true) to the local variable
+function passFromStatus(section) {
+	switch(section) {
+		case 'work':
+	        return workStatus;
+    	case 'education':
+	        return educationStatus;
+	    case 'project':
+	    	return projectStatus;
+	    case 'map':
+	    	return mapStatus;
+	    case 'publication':
+	    	return publicationStatus;
+	    case 'skills':
+	    	return skillsStatus;
+	    case 'contact':
+	    	return contactStatus;
+    	default:
+        	console.log('Error in setting status from boolean variable');
+        	break;
+	};
+}
+
+//sets the status of the section display property (i.e. none=false; block=true) from teh local variable
+function passToStatus(section, status) {
+	switch(section) {
+		case 'skills':
+	        skillsStatus = status;
+	        break;
+	    case 'contact':
+	        contactStatus = status;
+	        break;
+		case 'work':
+	        workStatus = status;
+	        break;
+    	case 'education':
+	        educationStatus = status;
+	        break;
+	    case 'project':
+	    	projectStatus = status;
+	    	break;
+	    case 'map':
+	    	mapStatus = status;
+	    	break;
+	    case 'publication':
+	    	publicationStatus = status;
+	    	break;
+    	default:
+        	console.log('Error in passing status back to boolean variable');
+        	break;
+	};
+}
+
+//function to get pubmed array of publication ids
+function getPublications() {
+	var pubmedSearchAPI = "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?";
+	var pubmedSummaryAPI ="http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?";
+	var database = "pubmed";
+	var returnmode = "json";
+	var returnmax = "100";
+	var searchterm = "ansley+l[author]";
+	var returntype = "abstract";
+
+	getPubmedId(pubmedSearchAPI,database,returnmode,returnmax,searchterm,pubmedSummaryAPI,returntype);
+};
+
+function getPubmedId(searchAPI,database,returnmode,returnmax,searchterm,abstractAPI,returntype) {
+
+	var ids = [];
+
+	$.get(searchAPI,{
+		db: database,
+		retmode: returnmode,
+		retmax: returnmax,
+		term: searchterm
+	})
+	.done(function(data) { //when the get is complete then do the next steps
+		var ids = data.esearchresult.idlist;
+    	var publications = [];
+    	getPubmedReferences(ids,database,returnmode,abstractAPI,returntype);
+    });
+};
+
+function getPubmedReferences(idList,database,returnMode,abstractAPI,returnType) {
+   	var str = idList[0].toString();
+    var idStringList = idList.toString();
+    var jqxhr = $.get(abstractAPI,{
+    	db: database,
+    	retmode: returnMode,
+    	retype: returnType,
+    	id: idStringList
+    })
+	.done(function(summary) {
+    	$('#publication').append(HTMLpublicationContainer);
+
+    	for(refs in summary.result) {
+
+			if(refs !== 'uids') {
+				var authors = '';
+				var publication = '';
+				//reverse order by prepending
+				$('#publication-container').prepend(HTMLpublicationStart);
+
+				var authorCount = ((summary.result[refs].authors).length);
+
+				var i=0;
+				while (i<authorCount-1) {
+					authors += summary.result[refs].authors[i].name + ', ';
+					i++;
+				}
+
+				publication = HTMLpublication.replace('%data%','http://www.ncbi.nlm.nih.gov/pubmed/'+refs)
+				authors += summary.result[refs].lastauthor;
+				publication = publication.replace('%authors%',authors);
+				publication = publication.replace('%title%',summary.result[refs].title);
+				publication = publication.replace('%journal%',summary.result[refs].source);
+				//Alter formatting if articel is In Press
+				if(summary.result[refs].volume!=='') {
+					publication = publication.replace('%volume%',' ' + summary.result[refs].volume);
+					publication = publication.replace('%issue%','(' + summary.result[refs].issue + '): ');
+					publication = publication.replace('%pages%',summary.result[refs].pages +', ');
+					var date = summary.result[refs].pubdate.slice(0,4);
+					publication = publication.replace('%date%',date + '.');
+				}
+				else {
+					publication = publication.replace('%volume%','');
+					publication = publication.replace('%issue%','');
+					publication = publication.replace('%pages%','');
+					publication = publication.replace('%date%','');
+					publication = publication + ' In Press.'
+				}
+				$('.publication-entry:first').append(publication);
+			}
+		}
+		document.getElementById('publication-container').style.display = 'none';
+    });
+}
+
+//run these scripts when the form loads
+function onFormLoad() {
+}
